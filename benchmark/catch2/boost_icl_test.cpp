397b0d05b40ca988f663f09f780ce7f4d7022128
Voursstrreds <56487501+Voursstrreds@users.noreply.github.com>
2022-10-07 12:02:46 +0300

    Add minimal Catch2 benchmark (#24)
    * C++ compiler is added to dockerfile.
    
    * Delete Dockerfile
    
    * Delete devcontainer.json
    
    * interval_set example usage is added. cpp file inserted into examples folder.
    
    * Small error corrected.
    
    * Dockerfile clang-tidy clang-format packages added. WORKDIR and COPY statements added.
    
    * corrected wrong command
    
    * workspace and copy statements added to dockerfile.
    
    * catch2 benchmark test is added.
    
    * Catch2 benchmark test cpp file updated.
    
    * Single statement tests added.
    
    * Statements updated to  use rvalue only.
    
    * Finally I understood which issue I should read before committing.
    
    * typo
    
    * reverting and rebasing - clang- files deleted.
    
    * Dockerfile turned into default.
    
    * clang files are deleted. They already exist in main branch.
    
    * Dockerfile clang-tidy clang-format packages added. WORKDIR and COPY statements added.
    
    * corrected wrong command
    
    * workspace and copy statements added to dockerfile.
    
    * catch2 benchmark test is added.
    
    * Catch2 benchmark test cpp file updated.
    
    * Single statement tests added.
    
    * Statements updated to  use rvalue only.
    
    * Finally I understood which issue I should read before committing.
    
    * typo
    
    * reverting and rebasing - clang- files deleted.
    
    * Dockerfile turned into default.
    
    * clang files are deleted. They already exist in main branch.
    
    * .clang files reverted.
    
    * Reverting clang files

---
+++ benchmark/catch2/boost_icl_test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <boost/icl/interval.hpp>
+#include <boost/icl/interval_set.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+
+
+static boost::icl::interval<double>::type interval1(0.0, 100.0);
+static boost::icl::interval<double>::type interval2(10.0, 90.0);
+
+
+static boost::icl::interval_set<double> interval_set1(interval1);
+static boost::icl::interval_set<double> interval_set2(interval2);
+
+
+TEST_CASE("main") {
+
+    BENCHMARK("Add") {
+        interval_set1 + interval_set2;
+        return 0;
+    };
+
+    BENCHMARK("Subtract") {
+        interval_set1 - interval_set2;
+        return 0;
+    };
+
+    BENCHMARK("Contains") {
+        boost::icl::contains(interval_set1, interval_set2);
+        return 0;
+    };
+    
+}
+
+
+
+
+
+
+
+
+
+
+
