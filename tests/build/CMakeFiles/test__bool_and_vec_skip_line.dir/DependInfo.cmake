bb17860bd0c2fbba85e7f8f4fd5a4e9574dce47f
Voursstrreds <mehmetsaracoglu98@yahoo.com>
2022-11-13 03:17:40 +0300

    Skip line algorithm is implemented.

---
+++ tests/build/CMakeFiles/test__bool_and_vec_skip_line.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/bool_and_vec_skip_line.cpp" "/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/build/CMakeFiles/test__bool_and_vec_skip_line.dir/bool_and_vec_skip_line.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include/rvstd"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
