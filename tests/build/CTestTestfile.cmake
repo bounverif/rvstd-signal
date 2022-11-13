cf629338a97a0bb83b37bb43e2997f4a20adf412
Voursstrreds <mehmetsaracoglu98@yahoo.com>
2022-11-13 03:18:04 +0300

    Skip line algorithm is implemented.

--- tests/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests
-# Build directory: /home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ctest_bool_and_vec_skip_line "/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/build/test__bool_and_vec_skip_line")
-set_tests_properties(ctest_bool_and_vec_skip_line PROPERTIES  _BACKTRACE_TRIPLES "/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/CMakeLists.txt;14;add_test;/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/CMakeLists.txt;17;add_test_;/home/mehmet/Mehmet/Dersler/CMPE492/rvstd/tests/CMakeLists.txt;0;")
